fdddb3c081610ebd14111d420068edc35926981d
Girish Palya <girishji@gmail.com>
2023-12-15 11:19:46 +0100

    M  day12.solution.py

--- day12.solution.py
+++ day12.solution.py
@@ -2,24 +2,6 @@
 import functools
 import re
 
-# part 1
-# with open('day12.input.txt') as f:
-#     summ = 0
-#     for line in f:
-#         springs, count = line.split()
-#         count = [int(cn) for cn in count.split(',')]
-#         unknwn = [i for i, x in enumerate(springs) if x == '?']
-#         for cand in itertools.combinations(unknwn, sum(count) - springs.count('#')):
-#             spr = [ch if i not in cand else '#' for i, ch in enumerate(springs)]
-#             spr = ''.join(spr).replace('?', '.')
-#             cnt = [len(cn) for cn in spr.split('.') if cn]
-#             if cnt == count:
-#                 summ += 1
-# print(summ)
-
-# part 1 and 2
-
-
 @functools.cache
 def solve(springs, counts):
     if not counts:
