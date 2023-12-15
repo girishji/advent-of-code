3a1da6e5f76418517ddccdbc18d0121bb4569c5a
Girish Palya <girishji@gmail.com>
2023-12-15 11:16:26 +0100

    M  day1.solution.py

--- day1.solution.py
+++ day1.solution.py
@@ -1,14 +1,5 @@
 import re
 
-# pat = re.compile(r'\d')
-# sum = 0
-# with open('day1.input') as f:
-#     for l in f:
-#         nums = pat.findall(l)
-#         if nums:
-#             sum += int(nums[0]) * 10 + int(nums[-1])
-#             print(nums, sum)
-
 snums = [ 'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten' ]
 pat = re.compile(r'(?=(\d|{}))'.format('|'.join(snums)))
 digit = lambda x: int(x) if re.search(r'\d', x) else (snums.index(x) % 10)
@@ -18,5 +9,4 @@ with open('day1.input.txt') as f:
         nums = pat.findall(l)
         if nums:
             sum += digit(nums[0]) * 10 + digit(nums[-1])
-            # print(nums, sum)
 print(sum)
